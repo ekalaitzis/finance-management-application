fd00e521495f710454e5cfea03af20340e21988f
Ace <kalaitzisemmanouil@gmail.com>
2026-05-13 21:45:23 +0200

    create very basic object structure

---
+++ Financial.py
@@ -0,0 +1,94 @@
+
+
+
+class Family:
+    def __init__(self)
+        
+
+    pass
+
+    def __str(self):
+
+        pass
+
+
+
+
+    def createFamily():
+        pass
+    def updateFamily():
+        pass
+    def printFamily():
+        pass
+    def deleteFamily():
+        pass
+    
+class Member:
+    def __init__(self)
+        
+
+    pass
+
+    def __str(self):
+
+        pass
+
+    
+    def createMember():
+        pass
+    def updateMember():
+        pass
+    def printMember():
+        pass
+    def deleteMember():
+        pass
+
+class Category:
+    def __init__(self)
+        
+
+    pass
+
+    def __str(self):
+
+        pass
+
+    
+    def createCategory():
+        pass
+    def updateCategory():
+        pass
+    def printCategory():
+        pass
+    def deleteCategory():
+        pass
+
+class Transaction:
+    def __init__(self)
+        
+
+    pass
+
+    def __str(self):
+
+        pass
+    
+    
+    def createTransaction():
+        pass
+    def updateTransaction():
+        pass
+    def printTransaction():
+        pass
+    def deleteTransaction():
+        pass
+
+
+
+
+
+
+
+
+def main():
+    print("Hello")
