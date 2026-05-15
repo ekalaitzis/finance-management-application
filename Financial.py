3aa112baa880f55df15db31c332e4f9f424684b7
Ace <kalaitzisemmanouil@gmail.com>
2026-05-15 15:10:58 +0200

    improve schema, add .db dile, small changes in objects

--- Financial.py
+++ Financial.py
@@ -6,44 +6,28 @@ class Family:
         self.familyId = familyId
         self.familyName = familyName
 
-    def __str(self):
+    def __str__(self):
         return f"This is the {self.familyName} Family!"
 
         
 
-    def createFamily():
-        pass
-    def updateFamily():
-        pass
-    def printFamily():
-        pass
-    def deleteFamily():
-        pass
     
 class Member:
     def __init__(self, memberId,familyId, firstName, lastName, email, password):
         self.id = memberId
         self.family_id = familyId
-        self.first_name = firstName
-        self.last_name = lastName
+        self.firstName = firstName
+        self.lastName = lastName
         self.email = email
         self.password = password
 
-    def __str(self):
+    def __str__(self):
         m1 = "First name:" + str(self.firstName) + "\n"
         m2 = "Last name:" + str(self.lastName) + "\n"
         m3 = "Email:" + str(self.email)
 
         return m1 + m2 + m3
     
-    def createMember():
-        pass
-    def updateMember():
-        pass
-    def printMember():
-        pass
-    def deleteMember():
-        pass
 
 class Category:
     def __init__(self,categoryId, categoryName, memberId):
@@ -52,49 +36,29 @@ class Category:
         self.memberId = memberId
 
 
-    def __str(self):
+    def __str__(self):
         c1 = "Category:" + str(self.categoryName) + "\n"
         c2 = "Member Id:" + str(self.memberId)
         return c1 + c2
     
-    def createCategory():
-        pass
-    def updateCategory():
-        pass
-    def printCategory():
-        pass
-    def deleteCategory():
-        pass
 
 class Transaction:
     def __init__(self, transactionName, transactionType, amount, date, categoryId):
         self.transactionName = transactionName
         self.transactionType = transactionType
         self.amount = amount
-        self.date = date
+        self.date = date        # this should set to get current date and time maybe timestamp
         self.categoryId = categoryId
 
 
-    def __str(self):
+    def __str__(self):
         t1 = "Transaction name:" + str(self.transactionName) + "\n"
         t2 = "Transaction type:" + str(self.transactionType) + "\n"
         t3 = "Amount:" + str(self.amount) + "\n"
-        t4 = "Date:" + str(self.date) + "\n"
+        t4 = "Date:" + str(self.date) + "\n"  
         t5 = "Category Id:" + str(self.categoryId)
         return t1 + t2 + t3 + t4 + t5    
     
-    def createTransaction():
-        pass
-    def updateTransaction():
-        pass
-    def printTransaction():
-        pass
-    def deleteTransaction():
-        pass
-
-
-
-
 
 
 
