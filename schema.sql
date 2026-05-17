6af7bab884cfb6bdac97942c9a89419ba17e0de2
Ace <kalaitzisemmanouil@gmail.com>
2026-05-17 21:45:21 +0200

    change table attributes to be more readable, drop tables on execution to clean DB fast

--- schema.sql
+++ schema.sql
@@ -1,17 +1,19 @@
+DROP TABLE IF EXISTS "transaction";
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS member;
 
 CREATE TABLE member
 (
-    id                   INTEGER PRIMARY KEY,
-    first_name           TEXT NOT NULL,
-    last_name            TEXT NOT NULL,
-    username             TEXT NOT NULL UNIQUE,
-    password             TEXT NOT NULL
+    member_id           INTEGER PRIMARY KEY,
+    first_name          TEXT NOT NULL,
+    last_name           TEXT NOT NULL,
+    username            TEXT NOT NULL UNIQUE,
+    password            TEXT NOT NULL
 );
-
 
 CREATE TABLE category
 (
-    id                  INTEGER PRIMARY KEY,
+    category_id         INTEGER PRIMARY KEY,
     category_name       TEXT NOT NULL UNIQUE,
     member_id           INTEGER NOT NULL,
     CONSTRAINT fk_member
@@ -20,9 +22,9 @@ CREATE TABLE category
             ON DELETE CASCADE
 );
 
-CREATE TABLE transaction
+CREATE TABLE "transaction"
 (
-    id                  INTEGER PRIMARY KEY,
+    transaction_id      INTEGER PRIMARY KEY,
     transaction_name    TEXT NOT NULL,
     transaction_type    TEXT NOT NULL CHECK(transaction_type IN ('INCOME', 'EXPENSE')),
     amount              REAL NOT NULL CHECK(amount >= 0),
