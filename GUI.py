509c306923bbbc2a546f686b848fcc2753d1b8ef
panaalex2 <alexandros.panag94@gmail.com>
2026-05-18 15:49:11 +0100

    small changes

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from enum import member
 from tkinter import ttk
-
+import Financial as be
 
 # =========================
 # functions
@@ -8,7 +9,7 @@ from tkinter import ttk
 
 def show_register():
     login_fr.pack_forget()
-    main.geometry("400x500")
+    main.geometry("400x300")
     register_fr.pack()
 
 def show_login():
@@ -35,6 +36,9 @@ def show_expenses():
 
 def show_subscription():
     subscription_fr.tkraise()
+
+def new_registration():
+    print (str(first_name) + " " + str(last_name) + " " + str(username_new) + " " +  str(password_new) + " " +  str(password_new2) )
 
 # =========================
 # Main Window
@@ -86,6 +90,8 @@ register_fr = tk.Frame(main)
 
 # variables
 
+first_name = tk.StringVar()
+last_name = tk.StringVar()
 username_new = tk.StringVar()
 password_new = tk.StringVar()
 password_new2 = tk.StringVar()
@@ -93,25 +99,33 @@ password_new2 = tk.StringVar()
 # widgets
 
 Register_title_label = tk.Label ( register_fr, text="Account Creation\nInsert your information " ,font=( "arial",18,"bold"), padx=5, pady=5 )
-new_username_label = tk.Label(register_fr,text= "Create Username:", padx=5, pady=5, width=20)
+new_username_label = tk.Label(register_fr,text= "New Username:", padx=5, pady=5, width=20)
 new_username_entry = tk.Entry (register_fr, textvariable=username_new, width=20)
 new_password_label = tk.Label(register_fr, text = "Create Password:", padx=5, pady=5, width=20)
 new_password_entry= tk.Entry(register_fr, textvariable= password_new , show= "*", width=20)
 new_password2_label = tk.Label(register_fr, text = "Reenter Password:", padx=5, pady=5, width=20)
 new_password2_entry= tk.Entry(register_fr, textvariable= password_new2 , show= "*", width=20)
-create_button = tk.Button (register_fr, text= "Create", padx=5, pady=5)
+new_first_name_label = tk.Label(register_fr, text = "First Name:", padx=5, pady=5, width=20)
+new_first_name_entry= tk.Entry(register_fr, textvariable= first_name , width=20)
+new_last_name_label = tk.Label(register_fr, text = "Last Name:", padx=5, pady=5, width=20)
+new_last_name_entry= tk.Entry(register_fr, textvariable= last_name , width=20)
+create_button = tk.Button (register_fr, text= "Create", padx=5, pady=5, command=new_registration )
 back_button = tk.Button (register_fr, text= "Go back" , command= show_login, padx=5, pady=5)
 
 # grid
 Register_title_label.grid(row=0,columnspan=4)
-new_username_label.grid(row = 1, column = 0)
-new_username_entry.grid (row=1, column=1 , columnspan=2)
-new_password_label.grid(row=2,column=0)
-new_password_entry.grid (row=2, column=1 , columnspan=2)
-new_password2_label.grid(row=3,column=0)
-new_password2_entry.grid (row=3, column=1 , columnspan=2)
-create_button.grid (row= 4, column= 1)
-back_button.grid (row=4,column=2)
+new_first_name_label.grid(row = 1, column = 0)
+new_first_name_entry.grid(row=1, column=1 , columnspan=2)
+new_last_name_label.grid(row=2,column=0)
+new_last_name_entry.grid(row=2, column=1 , columnspan=2)
+new_username_label.grid(row = 3, column = 0)
+new_username_entry.grid (row=3, column=1 , columnspan=2)
+new_password_label.grid(row=4,column=0)
+new_password_entry.grid (row=4, column=1 , columnspan=2)
+new_password2_label.grid(row=5,column=0)
+new_password2_entry.grid (row=5, column=1 , columnspan=2)
+create_button.grid (row= 6, column= 1)
+back_button.grid (row=6,column=2)
 
 # =========================
 # Dashboard Frame
