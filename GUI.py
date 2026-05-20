f7e031466022ffd98d8612993e12edb6429e7aeb
panaalex2 <alexandros.panag94@gmail.com>
2026-05-20 12:28:23 +0100

    add login validation, add method to add transaction, added first chart

--- GUI.py
+++ GUI.py
@@ -1,7 +1,11 @@
 import tkinter as tk
-from enum import member
 from tkinter import ttk
+from tkinter import messagebox
+from tkcalendar import DateEntry
 import Financial as be
+import charts as chart
+import datetime
+
 
 # =========================
 # functions
@@ -18,9 +22,20 @@ def show_login():
     login_fr.pack()
 
 def show_dashboard():
-    login_fr.pack_forget()
-    main.geometry("1200x800")
-    dashboard_fr.pack(fill="both", expand=True)
+
+    if be.Member.validateMember(username_added.get(),password_added.get()):
+        login_fr.pack_forget()
+        main.geometry("1200x800")
+        dashboard_fr.pack(fill="both", expand=True)
+        user_information = be.Member.getMemberByUsername(username_added.get())
+        name_lbl.configure(text="Name: "+ user_information[1] + " " + user_information[2])
+        username_lbl.configure(text= "Username: " + user_information[3] )
+        user_id.configure(text= "User ID: " + str(user_information[0]))
+
+    else:
+        messagebox.showerror("Login Error", "Username or password is wrong.\nPlease try again.")
+        username_label.configure(fg="red")
+        password_label.configure(fg="red")
 
 def show_overview():
     overview_fr.tkraise()
@@ -36,6 +51,58 @@ def show_expenses():
 
 def show_subscription():
     subscription_fr.tkraise()
+
+def new_registration():
+    if password_new.get() == password_new2.get():
+        new_user = be.Member(first_name.get(), last_name.get(), username_new.get(), password_new.get())
+        success = new_user.createMember()
+
+        if success:
+            show_login()
+        else:
+            messagebox.showerror("Registration Failed", "User couldn't be created, \nPlease choose an other Username ")
+            new_username_label.configure(fg="red")
+    else:
+        messagebox.showerror("Password Error", "Passwords do not match.")
+        new_password_label.configure(fg= "red")
+        new_password2_label.configure(fg="red")
+
+def number_validation(value):# use to check the number is positive float
+    try:
+        amount_valid=float(value)
+
+        if amount_valid >= 0 :
+            return True
+        else:
+            return False
+
+    except ValueError:
+        return False
+
+def add_transaction():
+    if transaction_name_vr.get() == "" or transaction_type_vr.get() == "" or transaction_amount_vr.get() == "" or transaction_category_vr.get()=="":
+        messagebox.showerror("Empty Fields", "Please add all the required fields.")
+        transaction_amount_lbl.configure(fg= "red")
+        transaction_name_lbl.configure(fg= "red")
+        transaction_category_lbl.configure(fg= "red")
+        transaction_type_lbl.configure(fg= "red")
+    elif not number_validation(transaction_amount_vr.get()):
+        messagebox.showerror("Wrong Amount", "Please ensure you add a valid amount.\nEx:5.76")
+        transaction_amount_lbl.configure(fg= "red")
+        transaction_name_lbl.configure(fg= "black")
+        transaction_category_lbl.configure(fg= "black")
+        transaction_type_lbl.configure(fg= "black")
+    else:
+        transaction_amount_lbl.configure(fg= "black")
+        transaction_name_lbl.configure(fg= "black")
+        transaction_category_lbl.configure(fg= "black")
+        transaction_type_lbl.configure(fg= "black")
+    transaction_amount_vr.set("")
+    transaction_name_vr.set("")
+    transaction_category_vr.set("")
+    transaction_type_vr.set("")
+    transaction_date_entry.set_date(datetime.date.today())
+
 
 
 # =========================
@@ -62,7 +129,7 @@ password_added = tk.StringVar()
 
 # widgets
 
-login_title_label = tk.Label ( login_fr, text="Welcome! \n Please Enter Credentials" ,font=( "arial",18,"bold"), padx=5, pady=5 )
+login_title_label = tk.Label ( login_fr, text="Welcome!\nPlease Enter Credentials" ,font=( "arial",18,"bold"), padx=5, pady=5 )
 username_label = tk.Label(login_fr,text= "Username:", padx=5, pady=5, width=20)
 username_entry = tk.Entry (login_fr, textvariable= username_added, width=20)
 password_label = tk.Label(login_fr, text = "Password:", padx=5, pady=5, width=20)
@@ -132,23 +199,23 @@ back_button.grid (row=6,column=2)
 dashboard_fr = tk.Frame(main)
 
 # Header frame
-header_fr = tk.Frame (dashboard_fr, height=100 , width=1200)
+header_fr = tk.Frame (dashboard_fr, height=50 , width=1200)
 header_fr.pack_propagate(False)
 header_fr.pack (side= "top", fill= "both")
 
 # header widgets
 name_lbl = tk.Label(header_fr, text= "Name: Will retrieve auto", width= 30, anchor= "w", relief="groove")
 username_lbl = tk.Label(header_fr, text= "Username: Will retrieve auto", width= 30, anchor= "w", relief="groove")
-family_lbl = tk.Label(header_fr, text= "Family Name: Will retrieve auto" , width= 30, anchor= "w", relief="groove")
-account_nm = tk.Label(header_fr, text= "Account name: Will retrieve auto" , width= 30, anchor= "w", relief="groove")
-account_amount = tk.Label(header_fr, text= "example 3000:" , width= 30, anchor= "w", relief="groove")
+user_id = tk.Label(header_fr, text= "UserId: Will retrieve auto" , width= 30, anchor= "w", relief="groove")
+income_amount = tk.Label(header_fr, text= "Income: 3000" , width= 30, anchor= "w", relief="groove")
+expenses_amount = tk.Label(header_fr, text= "Expenses: 3000:" , width= 30, anchor= "w", relief="groove")
 
 # header position
-name_lbl.grid (row= 0 , column= 0 , padx=5 , pady= 5, sticky="w")
-username_lbl.grid (row= 0 , column= 1 , padx=5 , pady= 5, sticky="w")
-family_lbl.grid (row= 0, column= 2, padx=5 , pady= 5, sticky="w")
-account_nm.grid(row= 1 , column= 0 , padx=5 , pady= 5, sticky="w")
-account_amount.grid (row= 1 , column= 1 , padx=5 , pady= 5, sticky="w")
+name_lbl.grid (row= 0 , column= 1 , padx=5 , pady= 5, sticky="w")
+username_lbl.grid (row= 0 , column= 2 , padx=5 , pady= 5, sticky="w")
+user_id.grid (row= 0, column= 0, padx=5 , pady= 5, sticky="w")
+income_amount.grid (row= 1 , column= 0 , padx=5 , pady= 5, sticky="w")
+expenses_amount.grid (row= 1 , column= 1 , padx=5 , pady= 5, sticky="w")
 
 # Navigation frame
 
@@ -173,7 +240,7 @@ subscription_button.grid (row= 0 , column= 4 , padx=5 , pady= 5, sticky="w")
 # =========================
 # Content Frame
 # =========================
-basic_fr= tk.Frame (dashboard_fr, width=1200 , height=650,bg= "black")
+basic_fr= tk.Frame (dashboard_fr, width=1200 , height=700,bg= "black")
 basic_fr.pack_propagate(False)
 basic_fr.pack(fill= "both", expand=True)
 
@@ -181,16 +248,18 @@ basic_fr.pack(fill= "both", expand=True)
 # Overview Frame
 # =========================
 
-overview_fr= tk.Frame (basic_fr, width=1200 , height=650 , bg="purple")
+overview_fr= tk.Frame (basic_fr, width=1200 , height=700)
 overview_fr.grid_propagate(False)
 overview_fr.grid(row=0, column=0, sticky="nsew")
 overview_fr.columnconfigure(0,weight=3,uniform="group1")
-overview_fr.columnconfigure(1,weight=2,uniform="group1")
+overview_fr.columnconfigure(1,weight=1,uniform="group1")
+overview_fr.columnconfigure(2,weight=1,uniform="group1")
 overview_fr.rowconfigure(0, weight=1)
+overview_fr.rowconfigure(1, weight=1)
 overview_fr.tkraise()
 
 left_side_fr=tk.Frame(overview_fr)
-left_side_fr.grid(row=0, column=0, sticky="nsew")
+left_side_fr.grid(row=0, rowspan=2 ,column=0, sticky="nsew")
 left_side_fr.grid_rowconfigure(0, weight=1)
 left_side_fr.grid_columnconfigure(0, weight=1)
 
@@ -231,14 +300,49 @@ for row in random_data:
 left_table.grid(row= 0 , column= 0 , sticky= "nsew")
 
 # future pie chart
-right_side_fr=tk.Frame(overview_fr)
-right_side_fr.grid(row=0, column=1, sticky="nsew")
-right_side_fr.grid_rowconfigure(0, weight=1)
-right_side_fr.grid_columnconfigure(0, weight=1)
+top_right_side_fr=tk.Frame(overview_fr)
+top_right_side_fr.grid(row=0, column = 1, columnspan=2, sticky="nsew")
+top_right_side_fr.grid_rowconfigure(0, weight=1)
+top_right_side_fr.grid_columnconfigure(0, weight=1)
 
-chart_placeholder = tk.Label(right_side_fr, text="Future Chart Here")
-chart_placeholder.pack(expand=True)
+chart.income_vrs_expenses(top_right_side_fr)
 
+btm_right_side_fr=tk.Frame(overview_fr, relief="ridge", bd=2)
+btm_right_side_fr.grid(row=1, column=1, sticky="nsew",columnspan=2, padx=20, pady=20)
+btm_right_side_fr.grid_columnconfigure(0, weight=1)
+btm_right_side_fr.grid_columnconfigure(1, weight=1)
+#transaction
+transaction_name_vr = tk.StringVar()
+transaction_type_vr= tk.StringVar()
+transaction_amount_vr = tk.StringVar()
+transaction_category_vr= tk.StringVar()
+
+transaction_name_lbl = tk.Label(btm_right_side_fr,text= "Transaction:", width=30)
+transaction_name_entry = tk.Entry (btm_right_side_fr, textvariable=transaction_name_vr, width=30)
+transaction_type_lbl = tk.Label(btm_right_side_fr,text= "Type:",  width=30)
+transaction_type_entry= ttk.Combobox (btm_right_side_fr,  textvariable=transaction_type_vr , width=27)
+transaction_type_entry["values"]=("Income","Expense")
+transaction_type_entry.current()
+transaction_amount_lbl = tk.Label(btm_right_side_fr,text= "Amount:",  width=30)
+transaction_amount_entry = tk.Entry (btm_right_side_fr, textvariable=transaction_amount_vr, width=30)
+transaction_date_lbl= tk.Label(btm_right_side_fr,text= "Date:",  width=30)
+transaction_date_entry=DateEntry(btm_right_side_fr,width= 27, date_pattern = "dd-mm-yyyy")
+transaction_category_lbl= tk.Label(btm_right_side_fr,text= "Category:", width=30)
+transaction_category_entry= ttk.Combobox (btm_right_side_fr,  textvariable=transaction_category_vr , width=27)
+transaction_category_entry["values"]=("grocery","restaurant","salary")
+transaction_add_button=tk.Button(btm_right_side_fr, text= "Submit" , width=30 , command=add_transaction)
+
+transaction_name_lbl.grid(row=0,column=0,sticky="nw", padx=5, pady=5)
+transaction_name_entry.grid(row=0,column=1,sticky="ne", padx=5, pady=5)
+transaction_type_lbl.grid(row=1,column=0,sticky="nw", padx=5, pady=5)
+transaction_type_entry.grid (row=1,column=1,sticky="ne", padx=5, pady=5)
+transaction_amount_lbl.grid(row=2,column=0,sticky="nw", padx=5, pady=5)
+transaction_amount_entry.grid(row=2,column=1,sticky="ne", padx=5, pady=5)
+transaction_date_lbl.grid(row=3,column=0,sticky="nw", padx=5, pady=5)
+transaction_date_entry.grid(row=3,column=1,sticky="ne", padx=5, pady=5)
+transaction_category_lbl.grid(row=4,column=0,sticky="nw", padx=5, pady=5)
+transaction_category_entry.grid(row=4,column=1,sticky="ne", padx=5, pady=5)
+transaction_add_button.grid(row=5,column=1,sticky="ne", padx=5, pady=5)
 # =========================
 # Account Frame
 # =========================
